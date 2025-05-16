c45cbdfbd647fa53b092a5bc04f46621afd7d4bd
robaVin <64101828+robaVin@users.noreply.github.com>
2025-05-16 22:24:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ const io = socketIo(server);
 
 const i18n = require('i18n');
 
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+});
+
 // Performance middleware
 app.use(compression());
 app.use(helmet({
