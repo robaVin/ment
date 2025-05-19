99bee0f451607bf6184b4d0f32e1114e4ba4c382
robaVin <64101828+robaVin@users.noreply.github.com>
2025-05-20 02:39:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'https://www.npikem.com',
+  clientID: 'i8Z1flPMB03tSqOUT5FJeYoprBjufO71',
+  issuerBaseURL: 'https://dev-x517c5vxug2bmj6e.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
 const i18n = require('i18n');
 
 app.use((req, res, next) => {
